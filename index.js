07ab0ef31955b050480246b072a7bd5be5b002c9
azimuddin2 <mohammadazimuddin274@gmail.com>
2024-03-25 21:47:07 +0600

    volunteer network server app clone

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // database connection
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2n3vft3.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2n3vft3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -21,7 +24,7 @@ async function run() {
     try {
         const volunteersCollection = client.db('volunteerNetwork').collection('volunteers');
 
-        app.get('/volunteers', async(req, res) => {
+        app.get('/volunteers', async (req, res) => {
             const query = {};
             const volunteers = await volunteersCollection.find(query).toArray();
             res.send(volunteers);
@@ -30,7 +33,7 @@ async function run() {
     }
     finally {
     }
-}
+};
 run().catch(console.dir);
 
 
